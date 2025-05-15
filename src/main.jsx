e9fd02285ad00b40dd26960969d5b4a3f3678087
Mihiri Jayasena <122019143+Mihirirj@users.noreply.github.com>
2025-05-15 18:34:14 +0530

    Update main.jsx
    updated

--- src/main.jsx
+++ src/main.jsx
@@ -1,8 +1,8 @@
-// src/main.jsx
+
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App.jsx';
-import './index.css'; // Global styles
+import './index.css'; 
 import './i18n';     // i18next configuration
 
 ReactDOM.createRoot(document.getElementById('root')).render(
